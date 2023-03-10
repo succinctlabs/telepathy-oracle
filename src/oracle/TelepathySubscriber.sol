df84b365093a5168c8521cbfc9604fb4063bb767
mattstam <mattstam@live.com>
2023-03-10 12:00:19 -0800

    TestEventSubsciber done

--- src/oracle/TelepathySubscriber.sol
+++ src/oracle/TelepathySubscriber.sol
@@ -8,120 +8,127 @@ enum SubscriptionStatus {
 }
 
 struct SubscriptionData {
-    uint256 nonce;
     uint32 destinationChainId;
-    bytes32 destinationAddress;
+    address destinationAddress;
     address callbackAddress;
-    bytes eventSig;
+    bytes32 eventSig;
 }
 
-
 contract TelepathySubsciber {
-    event Subscribe(
-        uint256 indexed nonce,
-        address destinationContract,
-        bytes destinationCalldata,
-        address callbackContract,
-        bytes eventSig
-    );
+    event Subscribe(bytes32 indexed subscriptionId, SubscriptionData subscriptionData);
 
-    event Unsubscribe(
-        uint256 indexed nonce,
-        address destinationContract,
-        bytes destinationCalldata,
-        address callbackContract,
-        bytes eventSig
-    );
+    event Unsubscribe(bytes32 indexed subscriptionId, SubscriptionData subscriptionData);
 
-    error SubscriptionAlreadyActive(bytes32 subscriptionHash);
-    error SubscriptionNotActive(bytes32 subscriptionHash);
+    error SubscriptionAlreadyActive(bytes32 subscriptionId);
+    error SubscriptionNotActive(bytes32 subscriptionId);
 
-    uint256 public nextNonce = 1;
     mapping(bytes32 => SubscriptionStatus) public subscriptions;
 
-    function subscribe(uint32 _destinationChainId, bytes32 _destinationAddress, address _callbackAddress, bytes calldata _eventSig)
-        external
-        returns (uint256 nonce)
-    {
-        unchecked {
-            nonce = nextNonce++;
-        }
-
+    function subscribe(
+        uint32 _destinationChainId,
+        address _destinationAddress,
+        address _callbackAddress,
+        bytes32 _eventSig
+    ) external returns (bytes32 subscriptionId) {
         SubscriptionData memory subscriptionData =
-            SubscriptionData(nonce, _destinationChainId, _destinationAddress, _callbackAddress, _eventSig);
-        bytes32 subscriptionHash = keccak256(abi.encode(subscriptionData));
-        if(subscriptions[subscriptionHash] == SubscriptionStatus.SUBSCRIBED)  {
-            revert SubscriptionAlreadyActive(subscriptionHash);
-        }
-        subscriptions[subscriptionHash] = SubscriptionStatus.SUBSCRIBED;
+            SubscriptionData(_destinationChainId, _destinationAddress, _callbackAddress, _eventSig);
+        subscriptionId = keccak256(abi.encode(subscriptionData));
 
-        emit Subscribe(nonce, subscriptionHash, subscriptionData);
+        if (subscriptions[subscriptionId] == SubscriptionStatus.SUBSCRIBED) {
+            revert SubscriptionAlreadyActive(subscriptionId);
+        }
+        subscriptions[subscriptionId] = SubscriptionStatus.SUBSCRIBED;
+
+        emit Subscribe(subscriptionId, subscriptionData);
+
+        return subscriptionId;
     }
 
     /// @dev Only the original callbackAddress contract will be able to unsubscribe.
-    function unsubscribe(uint256 nonce, uint32 _destinationChainId, bytes32 _destinationAddress, bytes calldata _eventSig)
+    function unsubscribe(uint32 _destinationChainId, address _destinationAddress, bytes32 _eventSig)
         external
         returns (bytes32)
     {
         SubscriptionData memory subscriptionData =
-            SubscriptionData(nonce, _destinationChainId, _destinationAddress, msg.sender, _eventSig);
-        bytes32 subscriptionHash = keccak256(abi.encode(subscriptionData));
-        if(subscriptions[subscriptionHash] == SubscriptionStatus.UNSUBSCIBED)  {
-            revert SubscriptionNotActive(subscriptionHash);
+            SubscriptionData(_destinationChainId, _destinationAddress, msg.sender, _eventSig);
+        bytes32 subscriptionId = keccak256(abi.encode(subscriptionData));
+
+        if (subscriptions[subscriptionId] == SubscriptionStatus.UNSUBSCIBED) {
+            revert SubscriptionNotActive(subscriptionId);
         }
-        subscriptions[subscriptionHash] = SubscriptionStatus.UNSUBSCIBED;
-        
-        emit Unsubscribe(nonce, subscriptionHash, subscriptionData);
+        subscriptions[subscriptionId] = SubscriptionStatus.UNSUBSCIBED;
+
+        emit Unsubscribe(subscriptionId, subscriptionData);
     }
 }
 
-contract TestEventSubsciber {
-    bytes32 EVENT_SIG = keccak256("SentMessage(uint64,bytes32,bytes)");
+/// @notice This contract is used to subscribe to a cross-chain events from a destination contract.
+contract TestEventSubsciber is TelepathyHandler {
+    event CrossChainIncremented(uint256 value, address sender);
 
-    TelepathySubsciber public telepathySubsciber;
-    uint256 subscriptionId;
+    error InvalidDestinationChain(uint32 destinationChainId);
+    error InvalidDestinationAddress(address destinationAddress);
+    error InvalidSubscriptionId(bytes32 subscriptionId);
+    error InvalidEventSig(bytes32 eventSig);
 
-    constructor(address _telepathySubsciber, address _telepathyRouter) TelepathyHandler(_telepathyRouter) {
+    TelepathySubsciber immutable telepathySubsciber;
+    uint32 immutable EVENT_DESTINATION_CHAIN_ID;
+    address immutable EVENT_DESTINATION_ADDRESS;
+    bytes32 immutable EVENT_SIG; // e.g. keccak256("Incremented(uint256,address)");
+
+    bytes32 subscriptionId;
+
+    constructor(
+        address _telepathySubsciber,
+        address _telepathyRouter,
+        uint32 _destinationChainId,
+        address _destinationAddress,
+        bytes32 _eventSig
+    ) TelepathyHandler(_telepathyRouter) {
         telepathySubsciber = TelepathySubsciber(_telepathySubsciber);
+        EVENT_DESTINATION_CHAIN_ID = _destinationChainId;
+        EVENT_DESTINATION_ADDRESS = _destinationAddress;
+        EVENT_SIG = _eventSig;
     }
 
-    function subscribeToEvent(uint32 _destinationChainId, bytes32 _destinationAddress) external {
-        uint256 id = telepathySubsciber.subscribe(_destinationChainId, _destinationAddress, address(this), EVENT_SIG);
-        subscriptionId = id;
+    function subscribeToEvent(uint32 _destinationChainId, address _destinationAddress) external {
+        bytes32 subscribeId = telepathySubsciber.subscribe(
+            _destinationChainId, _destinationAddress, address(this), EVENT_SIG
+        );
+        subscriptionId = subscribeId;
     }
 
-    function unsubscribeFromEvent(uint32 _destinationChainId, bytes32 _destinationAddress) external {
-        telepathySubsciber.unsubscribe(subscriptionId, _destinationChainId, _destinationAddress, EVENT_SIG);
-    }
-
-    function handleTelepathyImpl(uint32 _sourceChain, address _senderAddress, bytes memory _data)
-        internal
-        override
+    function unsubscribeFromEvent(uint32 _destinationChainId, address _destinationAddress)
+        external
     {
-        (
-            uint256 nonce,
-            uint32 destinationChainId,
-            bytes32 destinationAddress,
-            address callbackAddress,
-            bytes memory eventSig,
-            bytes memory eventData
-        ) = abi.decode(_data, (uint256, address, bytes, address, bytes));
+        telepathySubsciber.unsubscribe(_destinationChainId, _destinationAddress, EVENT_SIG);
+    }
 
-        abi.encodeWithSignature(keccak256("SentMessage(uint64,bytes32,bytes)"), arg);
-        SubscriptionData memory subscriptionData =
-            SubscriptionData(nonce, _destinationChainId, _destinationAddress, _callbackAddress, _eventSig);
-        bytes32 subscriptionHash = keccak256(abi.encode(subscriptionData));
-        if(subscriptions[subscriptionHash] == SubscriptionStatus.UNSUBSCIBED)  {
-            revert SubscriptionNotActive(subscriptionHash);
+    function handleTelepathyImpl(
+        uint32 _destinationChain,
+        address _destinationAddress,
+        bytes memory _data
+    ) internal override {
+        if (_destinationChain != EVENT_DESTINATION_CHAIN_ID) {
+            revert InvalidDestinationChain(_destinationChain);
         }
 
-        callbackContract.call(
-            abi.encodeWithSelector(
-                IOracleCallbackReceiver.handleOracleResponse.selector,
-                nonce,
-                responseData,
-                responseSuccess
-            )
-        );
+        if (_destinationAddress != EVENT_DESTINATION_ADDRESS) {
+            revert InvalidDestinationAddress(_destinationAddress);
+        }
+
+        (bytes32 subId, bytes32 eventSig, bytes memory eventData) =
+            abi.decode(_data, (bytes32, bytes32, bytes));
+
+        if (subId != subscriptionId) {
+            revert InvalidSubscriptionId(subId);
+        }
+        if (eventSig != EVENT_SIG) {
+            revert InvalidEventSig(eventSig);
+        }
+
+        (uint256 value, address incrementer) = abi.decode(eventData, (uint256, address));
+
+        emit CrossChainIncremented(value, incrementer);
     }
 }
